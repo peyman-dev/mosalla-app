9fe7eb7e94f61dd809fc8e488dd770e5649b98b9
Peyman Ahmadi <peymangeek@gmail.com>
2026-02-20 15:51:01 +0330

    fixed sidebar

--- components/features/panel/common/sidebar.tsx
+++ components/features/panel/common/sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
-    return <aside className="h-dvh md:visible md:block hidden invisible! sticky top-0 right-0 w-[260px] p-5 border-e border-primary/20 bg-primary/10">
+    return <aside className="h-dvh md:visible! md:block! hidden invisible! sticky top-0 right-0 w-[260px] p-5 border-e border-primary/20 bg-primary/10">
         <header>
             <p className="text-2xl text-center font-bold text-primary">
                 پنل مدیریت
